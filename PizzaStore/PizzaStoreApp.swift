346ab3b511491a9b235e40921ff422d35fff84f2
bajajku <bajajku@sheridancollege.ca>
2025-01-28 10:27:56 -0500

    Initial Commit

---
+++ PizzaStore/PizzaStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaStoreApp.swift
+//  PizzaStore
+//
+//  Created by Kunal Bajaj on 2025-01-28.
+//
+
+import SwiftUI
+
+@main
+struct PizzaStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PizzaStoreTests/PizzaStoreTests.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaStoreTests.swift
+//  PizzaStoreTests
+//
+//  Created by Kunal Bajaj on 2025-01-28.
+//
+
+import Testing
+@testable import PizzaStore
+
+struct PizzaStoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
